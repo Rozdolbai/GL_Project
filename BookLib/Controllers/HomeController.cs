8f6c113b2b4062d7f79530bfebdda89b7575b53d
Unknown <vladyslav.valuiskyi@nure.ua>
2018-10-01 15:58:23 +0300

    initial commit

---
+++ BookLib/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookLib.Models;
+using BookLib.Data;
+
+namespace BookLib.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Books = _context.Books.ToList();
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Show(int id)
+        {
+            var book = _context.Books.Find(id);
+            ViewBag.User = _context.Users.Find(book.UserId);
+
+            return View(book);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
